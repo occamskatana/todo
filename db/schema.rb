b9bdf9af4f14b572c6ad03c3ad343de536182c8c
occamskatana <johngallweycarter@gmail.com>
2016-02-01 21:29:44 -0500

    auth added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202020444) do
+ActiveRecord::Schema.define(version: 20160202022627) do
 
   create_table "items", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20160202020444) do
     t.datetime "updated_at", null: false
     t.string   "email"
     t.string   "name"
+    t.string   "password"
+    t.string   "username"
   end
 
 end
